a084a6b6a72605ed2396464a4deb23ab41131d15
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-03-01 22:21:44 -0600

    Setup a postion of the sandbox. added navmesh for player

---
+++ The Crypt/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Crypt.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        Ray lastRay;
+        
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Debug.DrawRay(lastRay.origin,lastRay.direction * 100,Color.red);
+               
+            }
+            GetComponent<NavMeshAgent>().destination = target.position;
+
+        }
+    }
+
+}
